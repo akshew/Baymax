a84cc7309c272467b7d843af9c3419513ae9ad8a
Priyanshuuuu <167881010+Bre4d77@users.noreply.github.com>
2024-08-15 23:50:13 +0530

    Update baymax.js

--- baymax.js
+++ baymax.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('frontend')); 
 app.use(express.json());
 
-const genAI = new GoogleGenerativeAI("AIzaSyCknw7IQPP39UzK9huDscA5JsQwv5GOMeg");
+const genAI = new GoogleGenerativeAI("");
 
 const SYSTEM_INSTRUCTION = "Meet Baymax, your friendly and supportive AI assistant, designed with a warm, approachable demeanor and a comforting presence. Baymax combines empathy with advanced capabilities to offer you the best support. Whether you need medical advice, emotional comfort, practical help, or just a friendly chat, Baymax is here to assist with a gentle touch and a caring heart. With his versatile skills and soothing personality, Baymax is more than just an assistant—he’s your trusted companion in any situation.";
 
